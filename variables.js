5fff09e207e41f74706b431d06076e5bec140c46
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2023-01-24 23:03:11 +0545

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,25 @@
+//variables
+let a="nilhil";
+let b=5;
+console.log(a+b)
+console.log(typeof (a+b))
+
+
+//object
+const c= {
+    name: "nikhl",
+     rollno :22,
+    level : "bachelor"
+}
+c['name']="niraj"
+console.log(c)
+//we can't change object name but we can chane inside
+
+const dict = {
+    optimistic: "to thoink positive",
+    appriciate : "to know the worth",
+    hard :"tough"
+}
+console.log(dict.hard)
+
+//
